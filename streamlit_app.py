45cfd8588fc18067d09449bdb587689b8c6a4cbd
kannanaranba <132473373+kannanaranba@users.noreply.github.com>
2023-05-04 13:26:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,14 +38,6 @@ except URLError as e:
    streamlit.error()
 
 
-
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list_values ('from streamlit')")
